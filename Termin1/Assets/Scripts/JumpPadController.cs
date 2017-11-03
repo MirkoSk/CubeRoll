da6d9e406b5dbbd75d9e12ac5381bd6f84d3fad0
Mirko <mirkoskroch@gmail.com>
2017-11-03 16:06:24 +0100

    Jump Pad added

---
+++ Termin1/Assets/Scripts/JumpPadController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadController : MonoBehaviour {
+
+    [SerializeField]
+    private float force;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider hit) {
+        if (hit.tag.Contains("Player")) {
+            hit.GetComponent<Rigidbody>().AddForce(Vector3.up * force, ForceMode.Impulse);
+        }
+    }
+}
