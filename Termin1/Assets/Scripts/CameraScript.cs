32d0ff6813c46b4e78c7bb0cf5d0754f5b420d1d
Melanie Ramsch <M.Ramsch@online.de>
2017-11-03 15:52:50 +0100

    Termin1 added <3

---
+++ Termin1/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    //Varriables
+    private Vector3 offset;
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Cube");
+        offset = transform.position-player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Termin1/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+    //Variables 
+    public int number=42;
+    Rigidbody rb;
+
+    
+    // respawn
+    private Vector3 startPosition;
+    private Quaternion startRot;
+   
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        startPosition = rb.transform.position;
+        startRot = rb.transform.rotation;
+	}
+	
+	void Update () {
+        moveCube();
+        if( rb.position.y <= -10 ) respawn();
+	}
+
+    private void moveCube () {
+        if( Input.GetKey(KeyCode.D) ) {
+            rb.AddForce(Vector3.right*10);
+        }
+        if( Input.GetKey(KeyCode.A) ) {
+            rb.AddForce(Vector3.right * -10);
+        }
+        if( Input.GetKey(KeyCode.W) ) {
+            rb.AddForce(Vector3.forward * 10);
+        }
+        if( Input.GetKey(KeyCode.S) ) {
+            rb.AddForce(Vector3.forward * -10);
+        }
+    }
+    private void respawn() {
+        rb.position = startPosition;
+        rb.rotation = startRot;
+        rb.velocity = Vector3.zero;
+    }
+}
