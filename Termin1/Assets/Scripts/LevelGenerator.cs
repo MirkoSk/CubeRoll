77bb1a7ae37df1fc8c8208dd9cc481e06efec715
Melanie Ramsch <M.Ramsch@online.de>
2017-11-03 17:37:09 +0100

    prefabs for Lvel Prozessing added

--- Termin1/Assets/Scripts/LevelGenerator.cs
+++ Termin1/Assets/Scripts/LevelGenerator.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    //Tiles
+    [Header("Tileset 1")]
+    public GameObject tile1_1;
+    public GameObject tile1_2;
+    public GameObject tile1_3;
+    [Header("Tileset 2")]
+    public GameObject tile2_1;
+    public GameObject tile2_2;
+    public GameObject tile2_3;
+
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
