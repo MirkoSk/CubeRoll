91bf99f89e18080cd905df6c2fdf40066b1c3502
Melanie Ramsch <M.Ramsch@online.de>
2017-11-07 22:02:53 +0100

    Speed Plane Particles and other Materials changed

---
+++ Termin1/Assets/Scripts/TileTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTrigger : MonoBehaviour {
+
+	private void OnTriggerEnter (Collider other) {
+        if( other.tag == "Player" ) { 
+            //calls Method to add a new Tile
+            LevelGenerator gen = GameObject.Find("_SCRIPTS").GetComponent<LevelGenerator>();
+            gen.newTile(transform.parent.position);
+            //Disable the Trigger
+            this.enabled = false;
+        }
+    }
+}
