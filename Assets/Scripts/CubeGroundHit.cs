983a89a0d96fc2e0d5498c89ce91f373d0d8a147
Mirko <mirkoskroch@gmail.com>
2017-11-22 16:53:44 +0100

    Cube spawning particles when hitting something

---
+++ Assets/Scripts/CubeGroundHit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGroundHit : MonoBehaviour {
+
+    public GameObject cubeGroundHitPSPrefab;
+
+    private void Start() {
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        GameObject cubeGroundHitPS = Instantiate(cubeGroundHitPSPrefab, collision.contacts[0].point + Vector3.up * 0.3f, Quaternion.identity);
+    }
+}
