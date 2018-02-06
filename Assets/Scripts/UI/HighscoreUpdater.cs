dbef64cb27df1da015173c039c32d8e0ada18990
Mirko <mirkoskroch@gmail.com>
2018-02-06 21:00:39 +0100

    Highscore Particles

--- Assets/Scripts/UI/HighscoreUpdater.cs
+++ Assets/Scripts/UI/HighscoreUpdater.cs
@@ -24,6 +24,7 @@ public class HighscoreUpdater : MonoBehaviour
     [SerializeField] AnimationCurve animationCurve;
 
     TextMeshProUGUI highscoreText;
+    ParticleSystem highscoreParticleSystem;
     Vector3 originalCounterScale;
     #endregion
 
@@ -33,6 +34,7 @@ public class HighscoreUpdater : MonoBehaviour
     private void Start () 
 	{
         highscoreText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        highscoreParticleSystem = transform.GetChild(1).GetComponent<ParticleSystem>();
 
         originalCounterScale = transform.localScale;
 	}
@@ -43,6 +45,7 @@ public class HighscoreUpdater : MonoBehaviour
 	#region Public Functions
 	public void UpdateText()
     {
+        highscoreParticleSystem.Play();
         highscoreText.text = ScoreCounter.Instance.Highscore.ToString();
         LeanTween.scale(gameObject, transform.localScale * (1 + scaleAmount), scaleDuration * (1f / 3.5f)).setEase(LeanTweenType.easeOutBack).setOnComplete(() => {
             LeanTween.scale(gameObject, transform.localScale * (1 - scaleAmount), scaleDuration * (1.5f / 3.5f)).setEase(animationCurve).setLoopCount(2).setOnComplete(() =>
@@ -50,6 +53,7 @@ public class HighscoreUpdater : MonoBehaviour
                 LeanTween.scale(gameObject, originalCounterScale, scaleDuration * (1f / 3.5f)).setEase(LeanTweenType.easeInOutSine);
             });
         });
+
     }
 	#endregion
 }
