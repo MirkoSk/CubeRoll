4d5a31c2e3242e788eb028356e3a1d7f53c11cb2
Mirko <mirkoskroch@gmail.com>
2018-02-14 12:25:18 +0100

    Beautified the MP timer

--- Assets/Scripts/UI/TimeUpdater.cs
+++ Assets/Scripts/UI/TimeUpdater.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 using TMPro;
 
 /// <summary>
+/// Handles updating the time UI for the Multiplayer.
 /// 
+/// Author: Mirko Skroch
 /// </summary>
 public class TimeUpdater : MonoBehaviour 
 {
 
     #region Variable Declarations
+    // Visible in Inspector
+    [Space]
+    [Tooltip("Scale amount on change of the score counter in percent.")]
+    [Range(0f, 0.3f)]
+    [SerializeField]
+    float scaleAmount = 0.1f;
+    [Tooltip("Scale duration on change of the score counter in seconds.")]
+    [Range(0f, 1f)]
+    [SerializeField]
+    float scaleDuration = 0.3f;
+
+    // Private Variables
+    // Configuration
+    Vector3 originalCounterScale;
+    Vector3 targetScale;
     MultiPlayerTimeController timeController;
     TextMeshProUGUI timeText;
+
+    // State
+    int currentRestTime;
 	#endregion
 	
 	
@@ -21,11 +41,20 @@ public class TimeUpdater : MonoBehaviour
 	{
         timeController = GameObject.FindObjectOfType<MultiPlayerTimeController>();
         timeText = transform.Find("Number").GetComponent<TextMeshProUGUI>();
-	}
+
+        currentRestTime = Mathf.CeilToInt(timeController.RestTime);
+        originalCounterScale = transform.localScale;
+        targetScale = originalCounterScale * (1 + scaleAmount);
+    }
 	
 	private void Update () 
 	{
-        timeText.text = Mathf.CeilToInt(timeController.RestTime).ToString();
+        int restTime = Mathf.CeilToInt(timeController.RestTime);
+        if (restTime != currentRestTime) {
+            currentRestTime = restTime;
+            timeText.text = currentRestTime.ToString();
+            LeanTween.scale(timeText.gameObject, targetScale, scaleDuration).setEase(LeanTweenType.punch);
+        }
 	}
 	#endregion
 }
