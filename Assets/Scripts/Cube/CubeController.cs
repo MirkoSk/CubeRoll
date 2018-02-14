0d95592d2ed3091adf9a5348f50c1652df1d5502
Mirko <mirkoskroch@gmail.com>
2018-02-14 11:32:49 +0100

    consistent points for SP

--- Assets/Scripts/Cube/CubeController.cs
+++ Assets/Scripts/Cube/CubeController.cs
@@ -145,7 +145,7 @@ public class CubeController : MonoBehaviour
 
 		StartCoroutine(Delay(references.deathParticleSystem.main.duration * 1.5f, () =>
         {
-			//NotifyScoreCounter();
+            if (Data.singlePlayerGame) NotifyScoreCounter();
 
 			if (!Data.singlePlayerGame)
             {
