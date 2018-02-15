50919dc0638c50072575f7e1dd644eade03b9f64
Mirko <mirkoskroch@gmail.com>
2018-02-15 12:05:33 +0100

    Returning to main menu when pressing escape anywhere in the game

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Returns to the main menu when pressing Escape.
+/// 
+/// Author: Mirko Skroch
+/// </summary>
+public class PauseMenu : MonoBehaviour 
+{
+    #region Unity Event Functions
+    private void Update()
+    {
+        if (Input.GetButtonDown(Constants.INPUT_ESCAPE) && SceneManager.GetActiveScene().buildIndex != Constants.MENU_SCENE)
+        {
+            SceneManager.LoadScene(Constants.MENU_SCENE);
+        }
+    }
+    #endregion
+}
